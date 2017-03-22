6b1901036d6856fd631910ddadcf5554a6156bc2
Jen O'Byrne <jenmobyrne@gmail.com>
2017-03-22 21:41:00 +1300

    Calculate if a year is a leap year

--- Leap.cs
+++ Leap.cs
@@ -1,9 +1,29 @@
-using System;
-
 public static class Leap
 {
     public static bool IsLeapYear(int year)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        if ((IsDivisbleByFour(year) 
+            && !IsDivisbleByOneHundred(year))
+            || IsDivisbleByFourHundred(year))
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    private static bool IsDivisbleByFour(int year)
+    {
+        return year % 4 == 0;
+    }
+
+    private static bool IsDivisbleByOneHundred(int year)
+    {
+        return year % 100 == 0;
+    }
+
+    private static bool IsDivisbleByFourHundred(int year)
+    {
+        return year % 400 == 0;
     }
 }
